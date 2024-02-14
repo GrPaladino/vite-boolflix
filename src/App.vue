98c49a4ef0ebf887d5506411bd36816d285a9170
GrPaladino <paladino.graziano@gmail.com>
2024-02-14 12:19:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+// import
+</script>
+
+<template>
+  <h1 class="container">ciao</h1>
+</template>
+
+<style lang="scss" scoped>
+@use "./styles/general.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+
+// * Import genreral scss
+import "./style/general.scss";
+
+// * Import bootstrap scss
+import * as bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
