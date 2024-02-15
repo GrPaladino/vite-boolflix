a6ec1d38bc43013f538083fe2df9134986766375
GrPaladino <paladino.graziano@gmail.com>
2024-02-15 11:46:42 +0100

    Fix: removed unused flags svg

--- src/store/index.js
+++ src/store/index.js
@@ -7,5 +7,5 @@ export const store = reactive({
     "https://api.themoviedb.org/3/search/movie?api_key=3cd49753d971acc438f6ec34b4197201&query=",
 
   tvShowApi:
-    "https://api.themoviedb.org/3/search/tv?&3cd49753d971acc438f6ec34b4197201",
+    "https://api.themoviedb.org/3/search/tv?api_key=3cd49753d971acc438f6ec34b4197201&query=",
 });
